48de72dcaf3569c4cbc2925944175372d0669075
carledwin <carledwin>
2018-02-02 11:54:12 -0200

    Configuração do Flyway.

---
+++ pessoaApi/src/main/resources/db/migration/SC2__script_insert_pessoa.sql
@@ -0,0 +1,3 @@
+insert into tb_pessoa values(null, 23145678412, 'Pedro Alves', 'Riviera');
+insert into tb_pessoa values(null, 35434344456, 'Amalia Antunes', 'Alonso');
+insert into tb_pessoa values(null, 78767665664, 'Fernanda', 'Nobrega');
--- pessoaApi/src/main/resources/db/migration/V1__script.sql
+++
@@ -1,4 +0,0 @@
-/*insert into tb_pessoa values(null, 'Nome0', 'Sobrenome0', 23145678412);
-insert into tb_pessoa values(null, 'Nome1', 'Sobrenome1', 35434344456);
-insert into tb_pessoa values(null, 'Nome2', 'Sobrenome2', 78767665664);
-*/
